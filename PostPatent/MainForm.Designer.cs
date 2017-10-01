dab671ea02065a6bb92d486c8cabc1c821a500e2
admin <admin@HUJINGBAO-PC>
2017-10-01 09:11:43 +0800

    添加项目文件。

---
+++ PostPatent/MainForm.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace PostPatent
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(62, 14);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(433, 21);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(517, 14);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "检索";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.webBrowser1.Location = new System.Drawing.Point(12, 57);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.ScriptErrorsSuppressed = true;
+            this.webBrowser1.Size = new System.Drawing.Size(684, 322);
+            this.webBrowser1.TabIndex = 2;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(684, 49);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "检索";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(708, 391);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.webBrowser1);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "获取专利信息";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ PostPatent/MainForm.cs
@@ -0,0 +1,87 @@
+﻿
+using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Security;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Web;
+
+namespace PostPatent
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// url
+        /// </summary>
+        private const string URL = "https://patentscope.wipo.int/search/zh/result.jsf";
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            Thread th = new Thread(new ThreadStart(Todo));
+            //启动线程
+            th.Start();
+        }
+
+        private  void Todo()
+        {
+            //加载页面
+            var web = new HtmlWeb();
+            HtmlAgilityPack.HtmlDocument doc = web.Load(URL);
+            HtmlNode rootnode = doc.DocumentNode;
+            //获取script 节点
+            HtmlNode node_script = rootnode.SelectSingleNode("//head/script[2]");
+            //获取script脚本节点的src值
+            string scriptSrc = node_script.Attributes["src"].Value;
+            //得到cookie
+            string cookie = scriptSrc.Substring(scriptSrc.LastIndexOf(";") + 1).Replace("jsessionid", "JSESSIONID");
+            //获取隐藏域节点
+            HtmlNode node_ViewState = rootnode.SelectSingleNode("//input[@name=\"javax.faces.ViewState\"]");
+            //得到隐藏域的值
+            string viewsatte = node_ViewState.Attributes["value"].Value;
+
+            StringBuilder postDataStr = new StringBuilder("javax.faces.ViewState="+viewsatte+"&resultListFormTop=resultListFormTo&resultListFormTop%3ArefineSearchField="+this.textBox1.Text.Trim()+"&resultListFormTop%3AgoToPage=1");
+
+            string html= HttpPost(URL, postDataStr.ToString(), cookie);
+
+            this.webBrowser1.Invoke((Action)(() => {
+                //this.webBrowser1.Navigate("about:blank");
+                this.webBrowser1.Navigate("about:blank");
+                this.webBrowser1.Document.Write(html);
+                this.webBrowser1.Refresh();
+            }));
+
+        }
+
+        private string HttpPost(string Url, string postDataStr, string  cookie)
+        {
+
+            WebClient webClient = new WebClient();
+            webClient.Headers.Add("content-Type", "application/x-www-form-urlencoded");//采取POST方式必须加的header，如果改为GET方式的话就去掉这句话即可  
+            webClient.Headers.Add("cookie", cookie);
+            webClient.Headers.Add("cache-control", "no-cache");
+            byte[] responseData = webClient.UploadData(Url, "POST", Encoding.UTF8.GetBytes(postDataStr));//得到返回字符流  
+            string srcString = Encoding.UTF8.GetString(responseData);//解码  
+            return srcString;
+         
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            this.webBrowser1.Navigate(URL);
+        }
+    }
+}
