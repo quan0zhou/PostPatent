5bf9a505cd9511c7819fe4469166d23c3c062850
周全 <17740839904@163.com>
2017-10-20 10:02:32 +0800

    跨线程访问代码优化

--- PostPatent/MainForm.cs
+++ PostPatent/MainForm.cs
@@ -30,55 +30,105 @@ namespace PostPatent
 
         private void button1_Click(object sender, EventArgs e)
         {
-            try
-            {
+      
                 Thread th = new Thread(new ThreadStart(Todo));
                 //启动线程
                 th.Start();
-            }
-            catch (Exception ex)
-            {
-
-                MessageBox.Show(ex.Message, "系统提示", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
+       
         
         }
 
         private  void Todo()
         {
-            //加载页面
-            var web = new HtmlWeb();
-            HtmlAgilityPack.HtmlDocument doc = web.Load(simpleSearchURL);
-            HtmlNode rootnode = doc.DocumentNode;
-            //获取script 节点
-            HtmlNode node_script = rootnode.SelectSingleNode("//head/script[2]");
-            //获取script脚本节点的src值
-            string scriptSrc = node_script.Attributes["src"].Value;
-            //得到cookie
-            string cookie = scriptSrc.Substring(scriptSrc.LastIndexOf(";") + 1).Replace("jsessionid", "JSESSIONID");
-            //获取隐藏域节点
-            HtmlNode node_ViewState = rootnode.SelectSingleNode("//input[@name=\"javax.faces.ViewState\"]");
-            //得到隐藏域的值
-            string viewsatte = node_ViewState.Attributes["value"].Value;
+            try
+            {
+                //加载页面
+                var web = new HtmlWeb();
+                HtmlAgilityPack.HtmlDocument doc = web.Load(simpleSearchURL);
+                HtmlNode rootnode = doc.DocumentNode;
+                //获取script 节点
+                HtmlNode node_script = rootnode.SelectSingleNode("//head/script[2]");
+                //获取script脚本节点的src值
+                string scriptSrc = node_script.Attributes["src"].Value;
+                //得到cookie
+                string cookie = scriptSrc.Substring(scriptSrc.LastIndexOf(";") + 1).Replace("jsessionid", "JSESSIONID");
+                //获取隐藏域节点
+                HtmlNode node_ViewState = rootnode.SelectSingleNode("//input[@name=\"javax.faces.ViewState\"]");
+                //得到隐藏域的值
+                string viewsatte = node_ViewState.Attributes["value"].Value;
 
-            StringBuilder postDataStr = new StringBuilder();
-            postDataStr.Append("simpleSearchSearchForm=simpleSearchSearchForm");
-            postDataStr.Append("&simpleSearchSearchForm:j_idt380="+this.comboBox1.SelectedValue);
-            postDataStr.Append("&simpleSearchSearchForm:fpSearch=" + this.textBox1.Text.Trim());
-            postDataStr.Append("&simpleSearchSearchForm:commandSimpleFPSearch=检索");
-            postDataStr.Append("&simpleSearchSearchForm:j_idt451=workaround");
-            postDataStr.Append("&javax.faces.ViewState=" + viewsatte);
+                StringBuilder postDataStr = new StringBuilder();
+                postDataStr.Append("simpleSearchSearchForm=simpleSearchSearchForm");
+                postDataStr.Append("&simpleSearchSearchForm:j_idt380=" + InvokeStr("combo"));
+                postDataStr.Append("&simpleSearchSearchForm:fpSearch=" + InvokeStr("text"));
+                postDataStr.Append("&simpleSearchSearchForm:commandSimpleFPSearch=检索");
+                postDataStr.Append("&simpleSearchSearchForm:j_idt451=workaround");
+                postDataStr.Append("&javax.faces.ViewState=" + viewsatte);
 
-            string html= HttpPost(simpleSearchURL, postDataStr.ToString(), cookie);
+                string html = HttpPost(simpleSearchURL, postDataStr.ToString(), cookie);
+                WebInvoke(html);
+            }
+            catch (Exception ex)
+            {
 
-            this.webBrowser1.Invoke((Action)(() => {
-                //this.webBrowser1.Navigate("about:blank");
+                MessageBox.Show(ex.Message,"系统提示", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+           
+        }
+        /// <summary>
+        /// 刷新页面
+        /// </summary>
+        /// <param name="html"></param>
+        private void WebInvoke(string html)
+        {
+            if (this.webBrowser1.InvokeRequired)
+            {
+                this.webBrowser1.Invoke((Action)(() => {
+                    this.webBrowser1.Navigate("about:blank");
+                    this.webBrowser1.Document.Write(html);
+                    this.webBrowser1.Refresh();
+                }));
+            }
+            else
+            {
                 this.webBrowser1.Navigate("about:blank");
                 this.webBrowser1.Document.Write(html);
                 this.webBrowser1.Refresh();
-            }));
+            }
 
         }
+
+        /// <summary>
+        /// 跨线程访问，返回控件的值
+        /// </summary>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        private string InvokeStr(string type)
+        {
+            if (type=="combo")
+            {
+                if (this.comboBox1.InvokeRequired)
+                {
+                    return this.comboBox1.Invoke((Func<string>)(() => { return this.comboBox1.SelectedValue.ToString(); })).ToString();
+                }
+                else
+                {
+                    return this.comboBox1.SelectedValue.ToString();
+                }
+            }
+            else if (type=="text")
+            {
+                if (this.textBox1.InvokeRequired)
+                {
+                    return this.textBox1.Invoke((Func<string>)(() => { return this.textBox1.Text.Trim(); })).ToString();
+                }
+                else
+                {
+                    return this.textBox1.Text.Trim();
+                }
+            }
+            return "";
+        } 
 
         private string HttpPost(string Url, string postDataStr, string  cookie)
         {
